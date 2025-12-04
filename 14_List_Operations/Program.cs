3f2acf5b46b301f6d44898e999e57c3179c74314
Woods <woodsleader@hotmail.com>
2025-12-04 13:56:09 +0100

    Exercise 14: list modifiers

---
+++ 14_List_Operations/Program.cs
@@ -0,0 +1,19 @@
+﻿List<string> inventory = new List<string>();
+inventory.Add("Master sword");
+inventory.Add("Bomb");
+inventory.Add("Potion");
+inventory.Add("Ancient artifact");
+inventory.Add("Bow");
+foreach (string s in inventory)
+{
+    Console.WriteLine(s);
+}
+Console.WriteLine();
+inventory.Sort();
+foreach (string s in inventory)
+{
+    Console.WriteLine(s);
+}
+inventory.Clear();
+Console.WriteLine("A bandit has robbed you of all your belongings!");
+Console.WriteLine($"Number of items in inventory: {inventory.Count}");
