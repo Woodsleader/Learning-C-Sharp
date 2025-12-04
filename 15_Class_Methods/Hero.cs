cc1bf1bf13c849193b980ff2dcdf74e0f81fe64f
Woods <woodsleader@hotmail.com>
2025-12-04 17:29:27 +0100

    Exercise 15: Class methods

---
+++ 15_Class_Methods/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _15_Class_Methods
+{
+    public class Hero
+    {
+        public string Name;
+        public int Health;
+
+        public void Heal(int amount)
+        {
+            this.Health += amount;
+        }
+        public void TakeDamage(int amount)
+        {
+            this.Health -= amount;
+            if (this.Health < 0)
+            {
+                this.Health = 0;
+            }
+        }
+    }
+}
