e8c4b132309096febaecdc0ca0a959c5ed80d786
WoodsLeader <woodsleader@hotmail.com>
2025-12-04 17:29:27 +0100

    Exercise 15: Class methods

---
+++ 15_Class_Methods/Program.cs
@@ -0,0 +1,9 @@
+﻿using _15_Class_Methods;
+
+Hero guts = new Hero();
+guts.Name = "Guts";
+guts.Health = 100;
+guts.TakeDamage(30);
+guts.TakeDamage(30);
+guts.Heal(10);
+Console.WriteLine($"{guts.Name}: {guts.Health}HP");
