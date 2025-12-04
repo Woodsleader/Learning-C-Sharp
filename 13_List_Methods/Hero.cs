9635c538fee23eec5d0aa6ebdb27aba0743e75ef
Woods <woodsleader@hotmail.com>
2025-12-04 13:28:22 +0100

    Exercise 13 complete, lists

---
+++ 13_List_Methods/Hero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13_List_Methods
+{
+    public class Hero
+    {
+        public string Name;
+        public int Health; 
+    }
+}
---
+++ 13_List_Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using _13_List_Methods;
+
+List<Hero> party = new List<Hero>();
+Hero character1 = new Hero();
+character1.Name = "Guts";
+character1.Health = 100;
+Hero character2 = new Hero();
+character2.Name = "Giffith";
+character2.Health = 100;
+Hero character3 = new Hero();
+character3.Name = "Caska";
+character3.Health = 100;
+party.Add(character1);
+party.Add(character2);
+party.Add(character3);
+foreach (Hero h in party)
+{
+    Console.WriteLine($"{h.Name}: {h.Health}HP");
+}
+party.Remove(character2);
+Console.WriteLine($"{character2.Name} betrayed your party and abandoned you...");
+character1.Health = 30;
+character2.Health = 200;
+character3.Health = 5;
+foreach (Hero h in party)
+{
+    Console.WriteLine($"{h.Name}: {h.Health}HP");
+}
