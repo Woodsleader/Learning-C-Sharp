a838dc01bc812a60cb6bd74f22d2ef59ac35763b
WoodsLeader <woodsleader@hotmail.com>
2025-12-04 19:51:26 +0100

    Started the RPG exercise

---
+++ 16_RPG_Exercise/Hero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _16_RPG_Exercise
+{
+    public class Hero
+    {
+        public string Name;
+        public int Health;
+    }
+}
