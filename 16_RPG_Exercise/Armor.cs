0e28de0c2fc5f2923104438adaa341ec62fe3d73
Woods <woodsleader@hotmail.com>
2025-12-05 11:38:07 +0100

    some changes in classes structures

--- 16_RPG_Exercise/Armor.cs
+++ 16_RPG_Exercise/Armor.cs
@@ -7,5 +7,12 @@ namespace _16_RPG_Exercise
     public class Armor : Item
     {
         public int ArmorStat;
+
+        public Armor(string name, int price, int armorStat)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.ArmorStat = armorStat;
+        }
     }
 }
