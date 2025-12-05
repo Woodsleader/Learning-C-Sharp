3384f576b99bc82c7fce86ea5de05f7d6fa4043d
WoodsLeader <woodsleader@hotmail.com>
2025-12-06 00:23:34 +0100

    Completed the buying feature

--- 16_RPG_Exercise/Inventory.cs
+++ 16_RPG_Exercise/Inventory.cs
@@ -7,15 +7,23 @@ namespace _16_RPG_Exercise
 {
     public class Inventory
     {
-        public List<Item> Items = new List<Item>();
+        public List<Item> characterInventory = new List<Item>();
+
+        public Inventory()
+        {
+            Sword rustySword = new Sword("Rusty sword", 0, 5);
+            characterInventory.Add(rustySword);
+            Armor leatherArmor = new Armor("Leather armor", 10, 3);
+            characterInventory.Add(leatherArmor);
+        }
 
         public void AddItem(Item item)
         {
-            this.Items.Add(item);
+            this.characterInventory.Add(item);
         }
         public void SellItem(Item item, Hero heroRecievingGold)
         {
-            this.Items.Remove(item);
+            this.characterInventory.Remove(item);
             heroRecievingGold.Gold += item.Price;
         }
     }
